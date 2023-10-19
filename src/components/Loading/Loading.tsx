cfaee3d9b40e7287c528a1b3f55ab495bef37094
kubraguler <oktnkbr@gmail.com>
2023-10-19 21:14:50 +0100

    Edit loading component style

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,11 +2,13 @@ import styles from "./Loading.module.scss";
 
 const Loading: React.FC = () => {
 	return (
-		<div className={styles.bouncer}>
-			<div className={styles["bouncer-item"]}></div>
-			<div className={styles["bouncer-item"]}></div>
-			<div className={styles["bouncer-item"]}></div>
-			<div className={styles["bouncer-item"]}></div>
+		<div className={styles["bouncer-wrapper"]}>
+			<div className={styles.bouncer}>
+				<div className={styles["bouncer-item"]}></div>
+				<div className={styles["bouncer-item"]}></div>
+				<div className={styles["bouncer-item"]}></div>
+				<div className={styles["bouncer-item"]}></div>
+			</div>
 		</div>
 	);
 };
